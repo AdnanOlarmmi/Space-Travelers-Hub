d4ffa68c5ff215515ba577c8de2f3c4c245abf21
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-14 13:19:35 +0100

    Add header and icon

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => (
+    <header>
+        <div>
+            <h1>
+                Space Travelers' Hub
+            </h1>
+            <img src='https://cdn-icons-png.flaticon.com/512/3212/3212608.png' alt='Space Travelers Hub Logo' />
+        </div>
+    </header>
+)

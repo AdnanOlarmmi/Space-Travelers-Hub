9df8650ec4159d3d783982bef92865361973dd45
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-14 13:51:15 +0100

    Style the Header nav

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+
+} from 'react-router-dom';
+import Header from './Header';
+
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+    </Routes>
+  </Router>
+);
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const Header = () => (
-    <header>
-        <div>
-            <h1>
+    <header className='flex-row'>
+        <div className='flex-row-center'>
+            <img className='icon' src='https://cdn-icons-png.flaticon.com/512/3212/3212608.png' alt='Space Travelers Hub Logo' />
+            <h5>
                 Space Travelers' Hub
-            </h1>
-            <img src='https://cdn-icons-png.flaticon.com/512/3212/3212608.png' alt='Space Travelers Hub Logo' />
+            </h5>
         </div>
+        <nav>
+            <ul className='nav flex-row'>
+                <li>Rocket</li>
+                <li>Missions</li>
+                <li>My Profile</li>
+            </ul>
+        </nav>
     </header>
 )
+
+export default Header;

d215df004a0ca45b8f5c5f6cd6ad441856bb7da8
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-15 12:02:13 +0100

    Fix Linter Error

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,27 +1,33 @@
 import React from 'react';
 
 const Rocket = (props) => {
-    const {description, name, image, status} = props;
+  const {
+    description, name, image,
+  } = props;
 
-    const onClick = (e) => {
-        e.target.previousElementSibling.childNodes[0].classList.toggle('hide');
-       e.target.classList.toggle('rocket__cancel');
-       if(e.target.classList.contains('rocket__cancel')) {
-        e.target.innerText = 'Cancel Reservation';
-       } else {
-        e.target.innerText = 'Reserve Rocket';
-       }
+  const onClick = (e) => {
+    e.target.previousElementSibling.childNodes[0].classList.toggle('hide');
+    e.target.classList.toggle('rocket__cancel');
+    if (e.target.classList.contains('rocket__cancel')) {
+      e.target.innerText = 'Cancel Reservation';
+    } else {
+      e.target.innerText = 'Reserve Rocket';
     }
+  };
 
-    return(
-    <section className='rocket flex-row'>
-        <img src={image} alt={name} className='rocket__img' />
-        <div className='rocket__info flex-column'>
-            <h4 className='rocket__heading'>{name}</h4>
-            <p className='rocket__description'><span className='hide'>Reserved</span>{description}</p>
-            <button className='rocket__reserve' onClick={onClick}>Reserve Rocket</button>
-        </div>
+  return (
+    <section className="rocket flex-row">
+      <img src={image} alt={name} className="rocket__img" />
+      <div className="rocket__info flex-column">
+        <h4 className="rocket__heading">{name}</h4>
+        <p className="rocket__description">
+          <span className="hide">Reserved</span>
+          {description}
+        </p>
+        <button type="button" className="rocket__reserve" onClick={onClick}>Reserve Rocket</button>
+      </div>
     </section>
-)}
+  );
+};
 
 export default Rocket;

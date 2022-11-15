d577b35f72c1112d70293bbc62df6737336c974d
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-15 11:02:07 +0100

    Make api call for rockets

---
+++ src/api/getRockets.js
@@ -0,0 +1,8 @@
+
+const getRockets = async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/rockets');
+    const rockets = await response.json();
+    return rockets;
+}
+
+export default getRockets;

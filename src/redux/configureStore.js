5d9f6b5cb2668edab6f87acf3e4302cf496fb919
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-15 10:18:45 +0100

    Complete rocket structure

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketReducer from './rockets/rocketSlice'
 
 const store = configureStore({
   reducer: {
+    rocketReducer,
   },
 });
 
